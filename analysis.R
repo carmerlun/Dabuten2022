e96ae7044c4bac313a8e7e184ac5cd5dc79ad8f8
carmerlun <carmerlun@gmail.com>
2022-09-19 17:14:53 +0200

    Add script for Dabuten analysis

---
+++ analysis.R
@@ -0,0 +1,12 @@
+###########################################
+### PROYECTO COLABORATIVO DABUTEN 2022  ###
+###########################################
+
+library(tidyverse)
+library(here)
+library(ggplot2)
+datos <- read.csv(here("data/github_data.csv"))
+plot(happiness ~ work.hours, data = datos)
+
+ggplot(datos) +
+  geom_point(aes(work.hours, happiness))
